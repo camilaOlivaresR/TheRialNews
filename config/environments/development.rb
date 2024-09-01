b7c99b1eb5f950e02e5617cba85c357e9e4a12fe
cami oliva <lamica.olivares@gmail.com>
2024-09-01 12:47:19 -0400

    add comment and figaro gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,21 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.perform_caching = false
+config.action_mailer.delivery_method = :sendmail
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default_options = {from: 'no-reply@testemail.com'}
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+address:'smtp.gmail.com',
+port:587,
+domain:'example.com',
+user_name: [Your Gmail Acc],
+password: [Your Gmail password],
+authentication:'plain',
+enable_starttls_auto: true
+}
+
 end

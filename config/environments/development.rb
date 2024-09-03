25e1ae7fb57919ae371e8b2fad36a1384305142e
cami oliva <lamica.olivares@gmail.com>
2024-09-03 17:28:38 -0400

    add author role, test , forgot password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,19 +77,19 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.perform_caching = false
-config.action_mailer.delivery_method = :sendmail
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default_options = {from: 'no-reply@testemail.com'}
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-address:'smtp.gmail.com',
-port:587,
-domain:'example.com',
-user_name: [Your Gmail Acc],
-password: [Your Gmail password],
-authentication:'plain',
-enable_starttls_auto: true
-}
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@testemail.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  user_name: ENV["user_name"],
+  password: ENV["password"],
+  address: 'sandbox.smtp.mailtrap.io',
+  host: 'sandbox.smtp.mailtrap.io',
+  port: '2525',
+  authentication: :login,
+  enable_starttls_auto: true
+  }
 
 end
